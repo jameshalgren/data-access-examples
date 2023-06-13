14894bc61b60490f60f22923b4d04332a6def157
James Halgren <jshalgren@ua.edu>
2023-06-13 17:36:32 -0500

    remove `model_output` from aws url

--- nwm_filenames/listofnwmfilenamesretro.py
+++ nwm_filenames/listofnwmfilenamesretro.py
@@ -15,6 +15,7 @@ retrospective_var_types = [
 ]
 objecttype = ["forcing/", "model_output/"]
 
+
 def generate_url(date, file_type, urlbase_prefix, data_type):
     year_txt = f"{date.strftime('%Y')}"
     date_txt = f"{date.strftime('%Y%m%d%H')}"
@@ -32,7 +33,10 @@ def validate_date_range(start_date, end_date):
     _until = datetime.strptime(end_date, "%Y%m%d%H%M")
     return _dtstart, _until
 
-def retrospective_archive_file_name_creator(start_date, end_date, objecttype, file_types, urlbase_prefix):
+
+def retrospective_archive_file_name_creator(
+    start_date, end_date, objecttype, file_types, urlbase_prefix
+):
     _dtstart, _until = validate_date_range(start_date, end_date)
     dates = rrule.rrule(
         rrule.HOURLY,
@@ -51,16 +55,28 @@ def retrospective_archive_file_name_creator(start_date, end_date, objecttype, fi
 
     return r, len(r)
 
+
 urlbasedict = {
     6: "https://noaa-nwm-retrospective-2-1-pds.s3.amazonaws.com/",
-    7: "s3://noaa-nwm-retrospective-2-1-pds/model_output/",
+    7: "s3://noaa-nwm-retrospective-2-1-pds/",
 }
 
-def create_file_list_retro(start_date=None, end_date=None, fcst_cycle=None, urlbaseinput=None, file_types=[1], objecttype=None):
+
+def create_file_list_retro(
+    start_date=None,
+    end_date=None,
+    fcst_cycle=None,
+    urlbaseinput=None,
+    file_types=[1],
+    objecttype=None,
+):
     urlbase_prefix = urlbasedict[urlbaseinput]
 
     if urlbaseinput == 6:
-        return retrospective_archive_file_name_creator(start_date, end_date, objecttype, file_types, urlbase_prefix)
+        return retrospective_archive_file_name_creator(
+            start_date, end_date, objecttype, file_types, urlbase_prefix
+        )
+
 
 def check_url(file):
     try:
@@ -70,27 +86,37 @@ def check_url(file):
     except requests.exceptions.RequestException:
         pass
 
+
 def check_valid_urls(file_list):
     with Pool(multiprocessing.cpu_count()) as p:
         valid_file_list = p.map(check_url, file_list)
     return [file for file in valid_file_list if file is not None]
 
+
 def main():
-    start_date = "19790201"
-    end_date = "19790202"
+    start_date = "19790202"
+    end_date = "19790203"
     fcst_cycle = [12, 18]
     urlbaseinput = 6
     file_types = [5, 6]
     objecttype = [1]
     start_time = "0000"
     end_time = "0800"
-    file_list, length = create_file_list_retro(start_date + start_time, end_date + end_time, fcst_cycle, urlbaseinput, file_types, objecttype)
+    file_list, length = create_file_list_retro(
+        start_date + start_time,
+        end_date + end_time,
+        fcst_cycle,
+        urlbaseinput,
+        file_types,
+        objecttype,
+    )
     if length == 0:
         print(f"No files found")
     else:
         print(f"Files: {file_list}\nTotal files: {len(file_list)}")
         valid_file_list = check_valid_urls(file_list)
         print(f"Valid Files: {valid_file_list}\nValid files: {len(valid_file_list)}")
+
 
 if __name__ == "__main__":
     main()
